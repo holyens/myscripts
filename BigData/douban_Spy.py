9d65d6c63568ac7d34608229eeb0345c8a1dd46d
zxc17 <644026981@qq.com>
2018-11-02 21:33:09 +0800

    new files

--- BigData/douban_Spy.py
+++ BigData/douban_Spy.py
@@ -14,11 +14,23 @@ def extractInfo(content=''):
         info_dict[key] = val
     return (title,info_dict)
 
+def paserRating(content=''):
+    title=re.findall(r'<h1>.*?<span.*?>(.*?)</span>.*?</h1>',content,re.S)[0]
+
+    info = re.findall(r'<div.*?rel="v:rating">(.*?)</div>',content,re.S)[0]
+    info_list = re.findall(r'<span class="pl".*?>(.*?):?</span>(.*?)(?:</span>)?<br/?>',info,re.S)
+    info_dict={}
+    for (key,val) in info_list:
+        key = whitespace.sub('', key)
+        val = whitespace.sub('', val)
+        info_dict[key] = val
+    return (title,info_dict)
+
 s = requests.Session()
 url = 'https://book.douban.com/subject/26425831/'
 content = s.get(url).text
 print(len(content))
 with open('G:/BigData/data/test.txt','w') as fp:
-    #fp.writelines(str(html))
+    fp.writelines(content)
     pass
 print(extractInfo(content))
